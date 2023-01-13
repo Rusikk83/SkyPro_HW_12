387d66664c2e75e546a2fe638b979295f247fa7c
AleshukinRV <rusikk@mai.ru>
2023-01-13 22:19:53 +0300

    Рабочая версия 1

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+import json
+
 from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
+from functions import *
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
@@ -9,22 +11,36 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_index():
-    pass
+    return render_template("index.html")
 
 
 @app.route("/list")
 def page_tag():
-    pass
+    find_text = request.args.get("s")
+    posts = posts_find(load_posts(), find_text)
+    return render_template("post_list.html", items=posts, find_text=find_text)
 
 
 @app.route("/post", methods=["GET", "POST"])
 def page_post_form():
-    pass
+    if request.method == "GET":
+        return render_template("post_form.html")
+    else:
+        picture = request.files.get("picture")
+        path = f"uploads/images/{picture.filename}"
+        picture.save(path)
+        content = request.values.get('content')
+        posts = load_posts()
+        posts.append({
+                       "pic": path,
+                       "content": content
+                     })
+        with open(file='posts.json', mode="w", encoding='utf-8') as file:
+            json.dump(posts, file, ensure_ascii=False)
+        return render_template("post_uploaded.html", img_path=path, content=content)
 
 
-@app.route("/post", methods=["POST"])
-def page_post_upload():
-    pass
+
 
 
 @app.route("/uploads/<path:path>")
@@ -33,4 +49,5 @@ def static_dir(path):
 
 
 app.run()
+
 
--- functions.py
+++ functions.py
@@ -1,1 +1,24 @@
 import json
+
+
+def load_posts(file_name="posts.json"):
+    with open(file_name, encoding='utf-8') as file:
+        data = file.read()
+    posts = json.loads(data)
+    return posts
+
+
+def posts_find(posts, find_text):
+    find_posts = []
+    for el in posts:
+        if find_text in el["content"]:
+            find_posts.append(el)
+    return find_posts
+
+
+postss = load_posts()
+
+print(postss)
+
+with open(file='posts.json', mode="w", encoding='utf-8') as file:
+    json.dump(postss, file, ensure_ascii=False)
